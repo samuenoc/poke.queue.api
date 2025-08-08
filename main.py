f0c74f1e4973af4e533c98b16af859d5b716a034
samuenoc <sreyesz@unah.hn>
2025-08-07 19:48:30 -0600

    chore: connection to the database

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import uvicorn
+import json
 from fastapi import FastAPI
+from utils.database import execute_query_json
 
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return { "project": "POKE QUEUE REQUEST" }
-
+    query = "select * from pokequeue.MESSAGES"
+    result = await execute_query_json(query)
+    result_dict = json.loads(result)
+    return result_dict
 @app.get("/api/version")
 async def version():
-    return { "version": "0.0.0" }
+    return { "version": "0.1.0" }
 
 
 if __name__ == "__main__":
